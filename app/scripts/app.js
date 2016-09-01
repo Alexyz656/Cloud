90fb33d4ecb328a4d8702bd24d5486d5240c5dcf
Alexyz656 <alexyz656@gmail.com>
2016-09-01 16:31:31 -0700

    restore commit

---
+++ app/scripts/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name projectsApp
+ * @description
+ * # projectsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('projectsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/tareas', {
+        templateUrl: 'views/tareas.html',
+        controller: 'TareasCtrl',
+        controllerAs: 'tareas'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/tareas.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projectsApp.controller:TareasCtrl
+ * @description
+ * # TareasCtrl
+ * Controller of the projectsApp
+ */
+angular.module('projectsApp')
+  .controller('TareasCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
